1e36a40fce926ddd8ed3fdd90af319913742271a
Calin Drule <calin@shiftstudio.com>
2024-02-10 22:41:09 +0200

    Created new project

---
+++ DemoPet/DemoPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoPetApp.swift
+//  DemoPet
+//
+//  Created by Calin Drule on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
